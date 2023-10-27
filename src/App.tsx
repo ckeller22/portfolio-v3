cc50d6689948f05ed260132d5d6920911a220519
ckeller22 <chris.keller.dev@gmail.com>
2023-10-26 20:24:57 -0500

    feat: add basic toggle for darkMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 // import EmailSideBar from './components/sidebar/EmailSideBar';
 // import SocialSideBar from './components/sidebar/SocialSideBars';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleClick = (event) => {
+    setDarkMode(!darkMode);
+    console.log('🚀 ~ file: App.tsx:11 ~ handleClick ~ darkMode:', darkMode);
+  };
+
   return (
-    <main className="h-full bg-zinc dark:bg-black">
-      <div>Some dummy content</div>
-    </main>
+    <div>
+      <button
+        type="button"
+        aria-label="switch"
+        className="py-2 px-4 bg-black text-white "
+        onClick={handleClick}
+      >
+        switch
+      </button>
+    </div>
   );
 }
 
