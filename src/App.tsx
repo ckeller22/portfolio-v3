79a4a383281c345e491bfd635072f96faa6a7f51
ckeller22 <chris.keller.dev@gmail.com>
2023-10-26 19:24:49 -0500

    feat: minor tw config change, color changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
-import EmailSideBar from './components/sidebar/EmailSideBar';
-import SocialSideBar from './components/sidebar/SocialSideBars';
+// import EmailSideBar from './components/sidebar/EmailSideBar';
+// import SocialSideBar from './components/sidebar/SocialSideBars';
 
 function App() {
   return (
-    <main>
-      <EmailSideBar />
-      <SocialSideBar />
+    <main className="h-full bg-zinc dark:bg-black">
+      <div>Some dummy content</div>
     </main>
   );
 }
--- src/components/shared/SideBar.tsx
+++ src/components/shared/SideBar.tsx
@@ -8,7 +8,7 @@ interface SideBarProps {
 function SideBar({ children, className }: SideBarProps) {
   const classes = `${
     className || ''
-  } hidden fixed bottom-0 mb-10 flex-col-reverse text-white 2xl:flex`;
+  } hidden fixed bottom-0 mb-10 flex-col-reverse text-slate-500 2xl:flex`;
 
   return <div className={classes}>{children}</div>;
 }
