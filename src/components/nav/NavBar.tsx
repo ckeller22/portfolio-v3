83eba85bf34a920bfaec37d04932d1ab9e9912f0
ckeller22 <chris.keller.dev@gmail.com>
2023-10-29 12:29:05 -0500

    feat: started work on nav list

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -93,13 +93,28 @@ function NavBar() {
               alt="Your Company"
             />
           </div>
-          <div>list</div>
+          <div>
+            <ul className="text-md flex flex-row rounded-full font-medium text-zinc-800 shadow-md ring-1 ring-zinc-900/5 dark:text-zinc-200 dark:ring-zinc-300/20">
+              <li className="rounded-l-full bg-white/90 py-2 pl-4 pr-3 dark:bg-zinc-800/90  ">
+                About
+              </li>
+              <li className=" bg-white/90 px-3 py-2  dark:bg-zinc-800/90 dark:ring-zinc-300/20">
+                Projects
+              </li>
+              <li className=" bg-white/90 py-2 pl-3  pr-4 dark:bg-zinc-800/90">
+                Experience
+              </li>
+              <li className="rounded-r-full bg-white/90 py-2 pl-3  pr-4 dark:bg-zinc-800/90">
+                Resume
+              </li>
+            </ul>
+          </div>
           <div>
             <button
               data-testid="theme-button"
               type="button"
               aria-label="switch"
-              className="relative rounded-full bg-white/90 px-3 py-2 text-teal-500 shadow-sm ring-1 ring-zinc-900/5 backdrop-blur hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20"
+              className="relative rounded-full bg-white/90 px-3 py-2 text-teal-500 shadow-md ring-1 ring-zinc-900/5 backdrop-blur hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20"
               onClick={handleClick}
             >
               {currentTheme === Theme.Dark ? (
