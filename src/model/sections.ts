880a07e50fc49fba2fe7d33918af05011ea690bb
ckeller22 <chris.keller.dev@gmail.com>
2023-11-01 13:30:34 -0500

    feat: add sectionData type and static data

---
+++ src/model/sections.ts
@@ -0,0 +1,25 @@
+type SectionData = {
+  id: string;
+  name: string;
+};
+
+const sections: Array<SectionData> = [
+  {
+    id: 'about-section',
+    name: 'About',
+  },
+  {
+    id: 'projects-section',
+    name: 'Projects',
+  },
+  {
+    id: 'experience-section',
+    name: 'Experience',
+  },
+  {
+    id: 'resume-section',
+    name: 'Resume',
+  },
+];
+
+export default sections;
