6bef2a5e69a24a8bafca98dce88c67ff21e8ba9f
ckeller22 <chris.keller.dev@gmail.com>
2023-11-03 13:50:18 -0500

    feat: impl CenteredContainer component

--- src/components/layout/CenteredContainer.tsx
+++ src/components/layout/CenteredContainer.tsx
@@ -4,9 +4,10 @@ interface CenteredContainerProps extends AppProps {}
 
 function CenteredContainer({ children }: CenteredContainerProps) {
   return (
-    <div>
-      CenteredContainer
-      {children}
+    <div className="mt-12 sm:px-8">
+      <div className="mx-auto w-full max-w-7xl lg:px-8">
+        <div className="px-4 sm:px-8 lg:px-12">{children}</div>
+      </div>
     </div>
   );
 }
