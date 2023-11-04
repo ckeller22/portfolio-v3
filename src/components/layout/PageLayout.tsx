3e27fdb9fc2325b9aa4895f325440418fb0ddd7a
ckeller22 <chris.keller.dev@gmail.com>
2023-11-04 13:08:28 -0500

    feat: work on work and avatars

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -46,29 +46,46 @@ function PageLayout() {
                   <Icon.Briefcase className="themed-icon-filled" />
                   <h2 className="themed-text-medium pl-4">Work</h2>
                 </div>
-                <ul className="flex flex-col">
+                <ul className="flex flex-col space-y-4">
                   {work.map((workData) => {
                     return (
                       <li key={workData.id} className="flex flex-row">
                         <div className="flex flex-shrink-0 items-center">
-                          <img
-                            className="h-10 w-10 rounded-full"
-                            src={workData.image}
-                            alt=""
-                          />
+                          {workData.image ? (
+                            <img
+                              className="h-10 w-10 rounded-full shadow-md "
+                              src={workData.image}
+                              alt=""
+                            />
+                          ) : (
+                            <div className="relative h-10 w-10 overflow-hidden rounded-full bg-zinc-100 shadow-md dark:bg-zinc-400">
+                              <svg
+                                className="absolute -left-1 h-12 w-12 text-zinc-400 dark:text-zinc-700"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </div>
+                          )}
                         </div>
-                        <dl className="ml-2 flex flex-grow flex-col">
+                        <dl className="ml-4 flex flex-grow flex-col">
                           <dt className="sr-only">Company</dt>
-                          <dd className="text-sm font-medium text-white">
+                          <dd className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
                             {workData.employer}
                           </dd>
                           <div className="flex flex-row justify-between">
                             <dt className="sr-only">Title</dt>
-                            <dd className="text-xs text-white">
+                            <dd className="text-xs text-zinc-500 dark:text-zinc-400">
                               {workData.position}
                             </dd>
                             <dt className="sr-only">Dates</dt>
-                            <dd className="text-xs text-white">
+                            <dd className="text-right text-xs text-zinc-400 dark:text-zinc-500">
                               {workData.duration}
                             </dd>
                           </div>
--- src/model/work.ts
+++ src/model/work.ts
@@ -2,7 +2,7 @@ import amerisaveIcon from '../images/amerisave-icon.jpg';
 
 export type WorkData = {
   id: string;
-  image: string;
+  image?: string;
   employer: string;
   position: string;
   duration: string;
@@ -15,6 +15,12 @@ const work: Array<WorkData> = [
     employer: 'AmeriSave Mortgage',
     position: 'Software Engineer II',
     duration: 'Feb 2022 - Oct 2023',
+  },
+  {
+    id: 'freelance',
+    employer: 'Freelance',
+    position: 'Full-stack Software Engineer',
+    duration: 'April 2020 - Present',
   },
 ];
 
