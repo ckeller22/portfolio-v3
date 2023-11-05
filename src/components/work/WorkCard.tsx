98e8d7d46ba9b5cd2ca6213847d2ff032b866f13
ckeller22 <chris.keller.dev@gmail.com>
2023-11-05 16:22:59 -0600

    feat: working on hover states

--- src/components/work/WorkCard.tsx
+++ src/components/work/WorkCard.tsx
@@ -25,11 +25,11 @@ function WorkCard() {
       <a
         href="/assets/christopher-keller-resume-10-26-2023.pdf"
         download
-        className="mt-6 flex w-full items-center justify-center gap-2 rounded-md bg-zinc-50 px-2 py-3 font-medium text-zinc-900 dark:bg-zinc-800/90 dark:text-zinc-300"
+        className="group mt-6 flex w-full items-center justify-center gap-2 rounded-md bg-zinc-50 px-2 py-3 font-medium text-zinc-900 transition hover:bg-zinc-100 dark:bg-zinc-800/90 dark:text-zinc-300 dark:hover:bg-zinc-700/90 dark:hover:text-zinc-200"
         type="button"
       >
         <span>Download Resume</span>
-        <Icon.ArrowDown className="h-4 w-4 stroke-zinc-400 dark:stroke-zinc-400" />
+        <Icon.ArrowDown className="h-4 w-4 stroke-zinc-400 dark:stroke-zinc-400 dark:group-hover:stroke-zinc-300" />
       </a>
     </Card>
   );
