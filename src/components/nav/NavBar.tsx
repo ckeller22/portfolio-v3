c36a293c4636fa02d5db2ece659c4c4a5ce6d8a8
ckeller22 <chris.keller.dev@gmail.com>
2023-11-07 10:26:18 -0600

    feat: refactor NavBar component styles and icons

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -2,11 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import portrait from '../../images/portrait.jpg';
 import sections, { SectionData } from '../../model/sections';
-import {
-  faCloudMoon,
-  faChevronDown,
-  faX,
-} from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faChevronDown, faX } from '@fortawesome/free-solid-svg-icons';
 import { faSun } from '@fortawesome/free-regular-svg-icons';
 import {
   DEFAULT_THEME,
@@ -160,11 +156,11 @@ function NavBar() {
                 data-testid="theme-button"
                 type="button"
                 aria-label="switch"
-                className="relative rounded-full bg-white/90 px-3 py-2 text-teal-400 shadow-md ring-1 ring-zinc-900/5 backdrop-blur transition hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20"
+                className="relative rounded-full bg-white/90 px-3 py-2 text-teal-400 shadow-md ring-1 ring-zinc-900/5 backdrop-blur transition hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20 dark:hover:bg-zinc-700/90 dark:hover:text-zinc-400"
                 onClick={handleClick}
               >
                 {currentTheme === Theme.Dark ? (
-                  <FontAwesomeIcon icon={faCloudMoon} size="xl" />
+                  <FontAwesomeIcon icon={faMoon} size="xl" />
                 ) : (
                   <FontAwesomeIcon icon={faSun} size="xl" />
                 )}
