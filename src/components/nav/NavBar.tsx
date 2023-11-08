7660fa8c8f08afdd73d44702eb46ecbdf6ab14a1
ckeller22 <chris.keller.dev@gmail.com>
2023-11-08 17:09:41 -0600

    feat: make navbar fixed, implement scrollHandler

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -3,6 +3,7 @@ import * as Dialog from '@radix-ui/react-dialog';
 import { faMoon, faChevronDown, faX } from '@fortawesome/free-solid-svg-icons';
 import { faSun } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { animateScroll } from 'react-scroll';
 import portrait from '../../images/portrait.jpg';
 import sections, { SectionData } from '../../model/sections';
 import {
@@ -11,10 +12,50 @@ import {
   Theme,
 } from '../../model/theme';
 import NavigationItem from './NavigationItem';
-import CenteredContainer from '../layout/CenteredContainer';
 
 function NavBar() {
   const [currentTheme, setCurrentTheme] = useState<Theme>(DEFAULT_THEME);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    // Define the scroll handler
+    const handleScroll = () => {
+      const currentScrollPosition = window.scrollY;
+
+      // Calculate the scroll difference
+      const scrollDifference = prevScrollPos - currentScrollPosition;
+
+      // Determine if the user has scrolled up more than 20 pixels
+      const hasScrolledUpSignificantly =
+        prevScrollPos > currentScrollPosition && scrollDifference > 20;
+
+      // Determine if the user is near the top of the page
+      const isNearTopOfPage = currentScrollPosition < 10;
+
+      // The navbar should be visible if the user has scrolled up significantly or is near the top of the page
+      const shouldNavbarBeVisible =
+        hasScrolledUpSignificantly || isNearTopOfPage;
+
+      // If the navbar visibility state needs to be updated, do so
+      if (isVisible !== shouldNavbarBeVisible) {
+        setIsVisible(shouldNavbarBeVisible);
+      }
+
+      // Update the previous scroll position state
+      setPrevScrollPos(currentScrollPosition);
+
+      console.log(isVisible);
+    };
+
+    // Attach the scroll handler
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup function to remove the scroll handler when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos, isVisible]); // Only re-run the effect if prevScrollPos or isVisible changes
 
   // Needs to be memoized to prevent infinite renders.
   const updateTheme = useCallback((theme: Theme) => {
@@ -85,6 +126,15 @@ function NavBar() {
     toggleTheme();
   };
 
+  const handleHomeClick = () => {
+    const options = {
+      duration: 500,
+      smooth: true,
+    };
+
+    animateScroll.scrollToTop(options);
+  };
+
   const desktopNavigationItems = sections.map((section: SectionData) => {
     return (
       <NavigationItem
@@ -102,71 +152,81 @@ function NavBar() {
   });
 
   return (
-    <CenteredContainer>
-      <div className="flex flex-row items-center justify-between pt-6">
-        <div className="flex flex-shrink-0 items-center">
-          <img
-            className="h-10 w-10 rounded-full shadow-md ring-1 ring-zinc-900/5 dark:ring-zinc-300/20"
-            src={portrait}
-            alt="Your Company"
-          />
-        </div>
-        <div className="flex flex-grow flex-row justify-end pr-4 sm:justify-center sm:pr-0">
-          {/* Mobile menu */}
-          <Dialog.Root>
-            <Dialog.Trigger asChild>
+    // The following divs replicate the structure of CenteredContainer
+    // but with support for fixed positioning.
+    <div className="fixed left-0 right-0 z-10 mx-auto w-full max-w-7xl p-4 lg:px-8">
+      <div className="mx-4 sm:mx-8 lg:mx-12">
+        <div className="relative mx-auto max-w-2xl lg:max-w-5xl">
+          <div className="flex flex-row items-center justify-between">
+            <button
+              type="button"
+              className="flex flex-shrink-0 items-center"
+              onClick={handleHomeClick}
+            >
+              <img
+                className="h-10 w-10 rounded-full shadow-md ring-1 ring-zinc-900/5 dark:ring-zinc-300/20"
+                src={portrait}
+                alt="Your Company"
+              />
+            </button>
+            <div className="flex flex-grow flex-row justify-end pr-4 sm:justify-center sm:pr-0">
+              {/* Mobile menu */}
+              <Dialog.Root>
+                <Dialog.Trigger asChild>
+                  <button
+                    type="button"
+                    className="font-md flex flex-row items-center rounded-full bg-white/90 px-3 py-2 text-sm font-medium text-zinc-800 shadow-md ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-zinc-300/20 sm:hidden"
+                  >
+                    Menu
+                    <FontAwesomeIcon
+                      icon={faChevronDown}
+                      className="ml-2 h-3 w-3"
+                    />
+                  </button>
+                </Dialog.Trigger>
+                <Dialog.Portal>
+                  <Dialog.Overlay asChild>
+                    <div className="fixed inset-0 z-50 bg-zinc-800/40 opacity-100 backdrop-blur-sm dark:bg-black/80" />
+                  </Dialog.Overlay>
+                  <Dialog.Content className="fixed inset-x-4 top-8 z-50 origin-top rounded-xl bg-white  shadow-md ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-zinc-300/20">
+                    <div className="p-6">
+                      <div className="text-md font-mediu flex flex-row justify-between">
+                        Navigation
+                        <Dialog.Close asChild>
+                          <FontAwesomeIcon icon={faX} className="h-6 w-6" />
+                        </Dialog.Close>
+                      </div>
+                      <ul className="text-md flex flex-col divide-y divide-zinc-100 pt-4 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                        {mobileNavigationItems}
+                      </ul>
+                    </div>
+                  </Dialog.Content>
+                </Dialog.Portal>
+              </Dialog.Root>
+              {/* Nav List Desktop */}
+              <ul className="text-md hidden flex-row overflow-hidden rounded-full bg-white/90 px-3 font-medium text-zinc-800 shadow-md ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-zinc-300/20 sm:flex">
+                {desktopNavigationItems}
+              </ul>
+            </div>
+            <div>
               <button
+                data-testid="theme-button"
                 type="button"
-                className="font-md flex flex-row items-center rounded-full bg-white/90 px-3 py-2 text-sm font-medium text-zinc-800 shadow-md ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-zinc-300/20 sm:hidden"
+                aria-label="switch"
+                className="relative rounded-full bg-white/90 px-3 py-2 text-teal-400 shadow-md ring-1 ring-zinc-900/5 backdrop-blur transition hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20 dark:hover:bg-zinc-700/90 dark:hover:text-zinc-400"
+                onClick={handleClick}
               >
-                Menu
-                <FontAwesomeIcon
-                  icon={faChevronDown}
-                  className="ml-2 h-3 w-3"
-                />
+                {currentTheme === Theme.Dark ? (
+                  <FontAwesomeIcon icon={faMoon} size="xl" />
+                ) : (
+                  <FontAwesomeIcon icon={faSun} size="xl" />
+                )}
               </button>
-            </Dialog.Trigger>
-            <Dialog.Portal>
-              <Dialog.Overlay asChild>
-                <div className="fixed inset-0 z-50 bg-zinc-800/40 opacity-100 backdrop-blur-sm dark:bg-black/80" />
-              </Dialog.Overlay>
-              <Dialog.Content className="fixed inset-x-4 top-8 z-50 origin-top rounded-xl bg-white  shadow-md ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-zinc-300/20">
-                <div className="p-6">
-                  <div className="text-md font-mediu flex flex-row justify-between">
-                    Navigation
-                    <Dialog.Close asChild>
-                      <FontAwesomeIcon icon={faX} className="h-6 w-6" />
-                    </Dialog.Close>
-                  </div>
-                  <ul className="text-md flex flex-col divide-y divide-zinc-100 pt-4 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                    {mobileNavigationItems}
-                  </ul>
-                </div>
-              </Dialog.Content>
-            </Dialog.Portal>
-          </Dialog.Root>
-          {/* Nav List Desktop */}
-          <ul className="text-md hidden flex-row overflow-hidden rounded-full bg-white/90 px-3 font-medium text-zinc-800 shadow-md ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-zinc-300/20 sm:flex">
-            {desktopNavigationItems}
-          </ul>
-        </div>
-        <div>
-          <button
-            data-testid="theme-button"
-            type="button"
-            aria-label="switch"
-            className="relative rounded-full bg-white/90 px-3 py-2 text-teal-400 shadow-md ring-1 ring-zinc-900/5 backdrop-blur transition hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20 dark:hover:bg-zinc-700/90 dark:hover:text-zinc-400"
-            onClick={handleClick}
-          >
-            {currentTheme === Theme.Dark ? (
-              <FontAwesomeIcon icon={faMoon} size="xl" />
-            ) : (
-              <FontAwesomeIcon icon={faSun} size="xl" />
-            )}
-          </button>
+            </div>
+          </div>
         </div>
       </div>
-    </CenteredContainer>
+    </div>
   );
 }
 
