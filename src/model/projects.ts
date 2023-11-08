324a29edb748897c11c945696510ebc673d79dfb
ckeller22 <chris.keller.dev@gmail.com>
2023-11-08 13:07:04 -0600

    chore: clean up some content

--- src/model/projects.ts
+++ src/model/projects.ts
@@ -4,7 +4,7 @@ export type ProjectData = {
   description: string;
   technologies: string[];
   githubURL: string;
-  liveURL: string | null;
+  liveURL?: string;
 };
 
 const projects: Array<ProjectData> = [
@@ -12,33 +12,38 @@ const projects: Array<ProjectData> = [
     id: 'portfolio-v3',
     title: 'Portfolio Website v3',
     description: 'This website, built using Typescript, React, and Tailwind.',
-    technologies: ['TypeScript', 'React', 'Tailwind'],
+    technologies: ['TypeScript', 'React', 'Radix UI', 'Tailwind', 'Vite'],
     githubURL: 'https://github.com/ckeller22/portfolio-v2',
-    liveURL: null,
   },
   {
     id: 'portfolio-v2',
     title: 'Portfolio Website v2',
     description: 'Previous portfolio website, built using React and Tailwind.',
-    technologies: ['Javascript', 'React', 'Tailwind'],
+    technologies: ['Javascript', 'React', 'Tailwind', 'Netlify'],
     githubURL: 'https://github.com/ckeller22/portfolio_v2',
-    liveURL: null,
   },
   {
     id: 'tixgit',
     title: 'TixGit',
     description:
-      'Scalable microservice app that allows users to sign up to view and purchase event tickets using Stripe API.',
-    technologies: ['Express', 'React', 'NextJS', 'Kubernetes'],
+      'Scalable microservice app that allows users to sign up to view, purchase, and sell event tickets using Stripe API.',
+    technologies: [
+      'TypeScript',
+      'Microservices',
+      'Express',
+      'React',
+      'NextJS',
+      'Kubernetes',
+      'Azure',
+    ],
     githubURL: 'https://github.com/ckeller22/microservice-ticketing-app',
-    liveURL: null,
   },
   {
     id: 'blogr',
     title: 'Blogr Landing Page',
     description:
-      'Responsive landing page, built with React and Tailwind CSS, hosted on Netlify. Solution for Blogr landing page challenge on Frontend Mentor.',
-    technologies: ['Javascript', 'React', 'Tailwind', 'Netlify'],
+      'Responsive landing page, built with React and Tailwind CSS, hosted with Vercel. Solution for Blogr landing page challenge on Frontend Mentor.',
+    technologies: ['Javascript', 'React', 'Tailwind', 'Vercel'],
     githubURL: 'https://github.com/ckeller22/blogr-landing-page',
     liveURL: 'https://blogr-landing-page-lilac.vercel.app/',
   },
@@ -46,10 +51,9 @@ const projects: Array<ProjectData> = [
     id: 'portfolio-v1',
     title: 'Portfolio Website v1',
     description:
-      'First version of my portfolio website with custom CSS. Hosted utlizing Heroku platform.',
-    technologies: ['CSS', 'SCSS', 'Javascript', 'PHP'],
+      'First version of my portfolio website with custom CSS. Previously hosted on Heroku.',
+    technologies: ['CSS', 'SCSS', 'Javascript', 'PHP', 'Heroku'],
     githubURL: 'https://github.com/ckeller22/Portfolio',
-    liveURL: 'https://portfolio-chris-keller.herokuapp.com/',
   },
   {
     id: 'wgu-term-tracker',
@@ -58,7 +62,6 @@ const projects: Array<ProjectData> = [
       'Android application to allow WGU students to plan their terms and course selections. Also allows user to take notes for courses and schedule reminder notifcations for upcoming assessments.',
     technologies: ['Android', 'Java', 'SQLite'],
     githubURL: 'https://github.com/ckeller22/WGU-Term-Tracker',
-    liveURL: null,
   },
   {
     id: 'wgu-crm',
@@ -67,7 +70,6 @@ const projects: Array<ProjectData> = [
       'JavaFX application intended to be used as a customer management system for a fictitious company. Allows users to manage and store both customer and appointment records.',
     technologies: ['Java', 'JavaFX', 'mySQL'],
     githubURL: 'https://github.com/ckeller22/Customer-Management-Software',
-    liveURL: null,
   },
 ];
 
