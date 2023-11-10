b8b7e2750bf2e570d8f1eae99d0c32bf363c2687
ckeller22 <chris.keller.dev@gmail.com>
2023-11-10 10:06:03 -0600

    feat: move theme toggle to own component

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
-import { faMoon, faChevronDown, faX } from '@fortawesome/free-solid-svg-icons';
-import { faSun } from '@fortawesome/free-regular-svg-icons';
+import { faChevronDown, faX } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { animateScroll } from 'react-scroll';
 import { debounce } from 'lodash';
@@ -13,6 +12,7 @@ import {
   Theme,
 } from '../../model/theme';
 import NavigationItem from './NavigationItem';
+import ToggleThemeButton from './ThemeToggleButton';
 
 function NavBar() {
   const [currentTheme, setCurrentTheme] = useState<Theme>(DEFAULT_THEME);
@@ -37,12 +37,12 @@ function NavBar() {
       }
 
       setPrevScrollPosition(currentScrollPosition);
-    }, 100); // Debounce time in milliseconds
+    }, 100);
 
     window.addEventListener('scroll', handleScroll);
 
     return () => {
-      handleScroll.cancel(); // Cancel any pending execution of handleScroll
+      handleScroll.cancel();
       window.removeEventListener('scroll', handleScroll);
     };
   }, [prevScrollPosition, isVisible]);
@@ -66,11 +66,6 @@ function NavBar() {
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
     setCurrentTheme(theme);
   }, []);
-
-  const toggleTheme = () => {
-    const newTheme = currentTheme === Theme.Light ? Theme.Dark : Theme.Light;
-    updateTheme(newTheme);
-  };
 
   const isValidTheme = (theme: string): theme is Theme => {
     return Object.values(Theme).includes(theme as Theme);
@@ -111,10 +106,6 @@ function NavBar() {
       updateTheme(theme);
     }
   }, [updateTheme]);
-
-  const handleClick = () => {
-    toggleTheme();
-  };
 
   const handleHomeClick = () => {
     const options = {
@@ -203,19 +194,10 @@ function NavBar() {
               </ul>
             </div>
             <div>
-              <button
-                data-testid="theme-button"
-                type="button"
-                aria-label="switch"
-                className="text-skin-primary relative rounded-full bg-white/90 px-3 py-2 shadow-md ring-1 ring-zinc-900/5 backdrop-blur transition hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20 dark:hover:bg-zinc-700/90 dark:hover:text-zinc-400"
-                onClick={handleClick}
-              >
-                {currentTheme === Theme.Dark ? (
-                  <FontAwesomeIcon icon={faMoon} size="xl" />
-                ) : (
-                  <FontAwesomeIcon icon={faSun} size="xl" />
-                )}
-              </button>
+              <ToggleThemeButton
+                currentTheme={currentTheme}
+                updateTheme={updateTheme}
+              />
             </div>
           </div>
         </div>
---
+++ src/components/nav/ThemeToggleButton.tsx
@@ -0,0 +1,37 @@
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useCallback } from 'react';
+import { Theme } from '../../model/theme';
+
+type ThemeToggleButtonProps = {
+  currentTheme: Theme;
+  updateTheme: (theme: Theme) => void;
+};
+
+function ThemeToggleButton({
+  currentTheme,
+  updateTheme,
+}: ThemeToggleButtonProps) {
+  const toggleTheme = useCallback(() => {
+    const newTheme = currentTheme === Theme.Light ? Theme.Dark : Theme.Light;
+    updateTheme(newTheme);
+  }, [currentTheme, updateTheme]);
+
+  return (
+    <button
+      data-testid="theme-button"
+      type="button"
+      aria-label="switch"
+      className="text-skin-primary relative rounded-full bg-white/90 px-3 py-2 shadow-md ring-1 ring-zinc-900/5 backdrop-blur transition hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20 dark:hover:bg-zinc-700/90 dark:hover:text-zinc-400"
+      onClick={toggleTheme}
+    >
+      {currentTheme === Theme.Dark ? (
+        <FontAwesomeIcon icon={faMoon} size="xl" />
+      ) : (
+        <FontAwesomeIcon icon={faSun} size="xl" />
+      )}
+    </button>
+  );
+}
+
+export default ThemeToggleButton;
