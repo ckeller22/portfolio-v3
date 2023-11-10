43bd6103eba509f17e4bc5f67d5e1f7696c7623d
ckeller22 <chris.keller.dev@gmail.com>
2023-11-10 09:44:22 -0600

    feat: got dynamic theme working

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -207,7 +207,7 @@ function NavBar() {
                 data-testid="theme-button"
                 type="button"
                 aria-label="switch"
-                className="relative rounded-full bg-white/90 px-3 py-2 text-teal-400 shadow-md ring-1 ring-zinc-900/5 backdrop-blur transition hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20 dark:hover:bg-zinc-700/90 dark:hover:text-zinc-400"
+                className="text-skin-primary relative rounded-full bg-white/90 px-3 py-2 shadow-md ring-1 ring-zinc-900/5 backdrop-blur transition hover:text-zinc-300  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-500 dark:shadow-none dark:ring-zinc-300/20 dark:hover:bg-zinc-700/90 dark:hover:text-zinc-400"
                 onClick={handleClick}
               >
                 {currentTheme === Theme.Dark ? (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 export default {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          primary: withOpacity('--color-primary'),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          primary: withOpacity('--color-primary'),
+        },
+      },
+      ringColor: {
+        skin: {
+          primary: withOpacity('--color-primary'),
+        },
+      },
+      borderColor: {
+        skin: {
+          primary: withOpacity('--color-primary'),
+        },
+      },
+    },
   },
   plugins: [],
 };
